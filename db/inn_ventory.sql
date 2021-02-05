8e3b90e9ca7255df50e2d02c1380d244d3a946d5
JamesSBeedle <jamessbeedle@gmail.com>
2021-02-05 18:55:48 +0000

    Initial commit

---
+++ db/inn_ventory.sql
@@ -0,0 +1,21 @@
+DROP TABLE suppliers;
+DROP TABLE products;
+
+CREATE TABLE products (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255),
+    category VARCHAR(255),
+    in_stock INT,
+    cost_price INT,
+    sale_price INT,
+    description TEXT
+);
+
+CREATE TABLE suppliers ( 
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255),
+    address VARCHAR(255),
+    phone_number INT,
+    product_id INT REFERENCES products(id) ON DELETE CASCADE
+
+)
