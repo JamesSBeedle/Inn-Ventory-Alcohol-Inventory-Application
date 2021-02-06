76357f77a31b6f2ef573815da296e309603234e3
JamesSBeedle <jamessbeedle@gmail.com>
2021-02-06 11:54:01 +0000

    added save function to supplier and product repositories

--- repositories/product_repository.py
+++ repositories/product_repository.py
@@ -3,3 +3,11 @@ from db.run_sql import run_sql
 from models.product import Product
 from models.supplier import Supplier
 import repositories.product_repository as product_repository
+
+def save(product):
+    sql = "INSERT INTO products (name, category, in_stock, cost_price, sale_price, description, supplier_id) VALUES (%s, %s, %s, %s, %s, %s, %s) RETURNING *"
+    values = [product.name, product.category, product.in_stock, product.cost_price, product.sale_price, product.description, product.supplier.id]
+    results = run_sql(sql, values)
+    id = results[0]['id']
+    product.id = id
+    return product
--- repositories/supplier_repository.py
+++ repositories/supplier_repository.py
@@ -3,3 +3,12 @@ from db.run_sql import run_sql
 from models.supplier import Supplier
 from models.product import Product
 import repositories.supplier_repository as supplier_repository
+
+
+def save(supplier):
+    sql = "INSERT INTO suppliers (name, address, phone_number, product_id) VALUES (%s, %s, %s, %s) RETURNING *"
+    values = [supplier.name, supplier.address, supplier.phone_number, supplier.product.id]
+    results = run_sql(sql, values)
+    id = results[0]['id']
+    supplier.id = id
+    return supplier
