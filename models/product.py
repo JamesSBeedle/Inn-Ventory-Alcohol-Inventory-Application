63bc9c0fe218b74fc386ca3469ad3ced3bed7756
JamesSBeedle <jamessbeedle@gmail.com>
2021-02-06 10:29:32 +0000

    models created

---
+++ models/product.py
@@ -0,0 +1,10 @@
+class Product:
+
+    def __init__(self, name, category, in_stock, cost_price, sale_price, description, supplier, id = None ):
+        self.name = name
+        self.category = category
+        self.in_stock = in_stock
+        self.cost_price = cost_price
+        self.sale_price = sale_price
+        self.description = description
+        self.id = id
---
+++ models/supplier.py
@@ -0,0 +1,8 @@
+class Supplier:
+
+    def __init__(self, name, address, phone_number, product, id = None):
+        self.name = name
+        self.address = address
+        self.phone_number = phone_number
+        self.product = product
+        self.id = id
