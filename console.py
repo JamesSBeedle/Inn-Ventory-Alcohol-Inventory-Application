858be0d1b2d147a4ba5e8f547419e535645abb36
JamesSBeedle <jamessbeedle@gmail.com>
2021-02-06 13:57:36 +0000

    modified console to provide more information to tables

--- console.py
+++ console.py
@@ -8,17 +8,22 @@ import repositories.supplier_repository as supplier_repository
 supplier1 = Supplier("Timothy Taylors", "Knowle Spring Brewery, Keighley", 669669, "Landlord")
 supplier_repository.save(supplier1)
 
+supplier2 = Supplier("ABV Wholesale", "Dalton Lane, Keighley", 699699, "Budvar")
+supplier_repository.save(supplier2)
+
+supplier3 = Supplier("Premier Cru", "Ballpark Road, Shipley", 545667, "Chenin Blanc")
+supplier_repository.save(supplier3)
+
+supplier_repository.select_all()
+
 product1 = Product("Landlord", "Draught", 12, 1.23, 3.00, "A Strong Pale Ale", supplier1)
 product_repository.save(product1)
 
+product2 = Product("Budvar", "Bottles", 24, 2.20, 4.50, "A Czech Pilsner", supplier2)
+product_repository.save(product2)
 
-
-
-
-
-
-
-
+product3 = Product("Chenin Blanc", "Wine", 12, 9.50, 16.50, "Crisp French White Wine", supplier3)
+product_repository.save(product3)
 
 
 pdb.set_trace()
